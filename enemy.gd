1606e1579304ab70a6d7bc63e18ae130a31f8cd8
Hookigan <jafiglietti@s.sfusd.edu>
2026-05-14 11:29:56 -0700

    Up4.1

--- enemy.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-@onready var player = $"../Player"
-@onready var Player = $"../Player"
-
-func _physics_process(delta: float) -> void:
-	look_at(player.global_position)
-	look_at(Player.global_position)
